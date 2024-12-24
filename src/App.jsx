f72f77b27a0140c22b61ad2c39e2c95e78770cc3
tonybnya <nya.tony2010@gmail.com>
2024-12-24 16:57:40 +0100

    feat: fetching Currencies from the Frankfurter API

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Converter from './components/Converter';
 import Footer from './components/Footer';
 
 const App = () => {
-  // https://api.frankfurter.dev/currencies
-  // https://api.frankfurter.dev/latest?amount=1&from=USD&to=EUR
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center font-bold text-4xl'>
       <Converter />
--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,8 +1,31 @@
-import Header from "./Header"
+import { useEffect, useState } from 'react';
+import Header from './Header';
 
 const Converter = () => {
+  // Currencies API URL: https://api.frankfurter.app/currencies
+  // Conversion API URL: https://api.frankfurter.app/latest?amount=1&from=USD&to=EUR
+  const [amount, setAmount] = useState(1);
+  const [currencies, setCurrencies] = useState([]);
+
+  const fetchCurrencies = async () => {
+    try {
+      const response = await fetch('https://api.frankfurter.app/currencies');
+      const data = await response.json();
+      setCurrencies(data);
+    //   setCurrencies(Object.keys(data));
+    } catch (error) {
+      console.error('Error fetching currencies:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
+  console.log(currencies);
+
   return (
-    <div className='rounded-lg shadow-2xl p-4 bg-white max-w-md mx-auto my-10 flex flex-col items-center justify-center'>
+    <div className='rounded-lg shadow-2xl p-4 bg-white max-lg:w-[80%] lg:w-1/2 mx-auto my-10 flex flex-col items-center justify-center'>
       <Header />
 
       {/* <div>dropdowns</div> */}
@@ -15,16 +38,17 @@ const Converter = () => {
           Amount:
         </label>
         <input
-          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
           type="number"
           className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:border-blue-500'
         />
       </div>
 
       <div className="flex justify-end mt-6">
-        <button class="group relative h-12 w-36 overflow-hidden rounded-xl bg-blue-500 text-lg font-bold text-white">
+        <button className="group relative h-12 w-36 overflow-hidden rounded-xl bg-blue-500 text-lg font-bold text-white">
           Convert
-          <div class="absolute inset-0 h-full w-full scale-0 rounded-xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
+          <div className="absolute inset-0 h-full w-full scale-0 rounded-xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
         </button>
       </div>
 
