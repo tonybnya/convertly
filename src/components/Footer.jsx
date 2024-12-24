39b54f88de26ccf4cde695cd4ef88bb3eee07de1
tonybnya <nya.tony2010@gmail.com>
2024-12-24 14:41:51 +0100

    style: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <p className='text-sm mt-4'>Built with <i className="fa-regular fa-heart text-red-500"></i> by <a href="https://github.com/tonybnya" target="_blank" rel="noopener noreferrer" className='text-blue-500'>tonybnya</a>.</p>
+  )
+}
+
+export default Footer;
