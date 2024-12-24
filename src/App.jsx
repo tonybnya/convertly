b98e5060aa6e20f90fa65c6fae09f9d122735c68
tonybnya <nya.tony2010@gmail.com>
2024-12-24 14:45:39 +0100

    style: insert Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Converter from './components/Converter';
+import Footer from './components/Footer';
 
 const App = () => {
   // https://api.frankfurter.dev/currencies
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center font-bold text-4xl'>
       <Converter />
+      <Footer />
     </div>
   )
 }
