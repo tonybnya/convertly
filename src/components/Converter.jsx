c4553f2eca106ae4860a7063f91b2e42ae939c03
tonybnya <nya.tony2010@gmail.com>
2024-12-24 14:44:58 +0100

    style: update Convert button

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png';
 
 const Converter = () => {
   return (
-    <div className='rounded-lg shadow-2xl p-4 bg-white max-w-xl mx-auto my-10 flex flex-col items-center justify-center'>
+    <div className='rounded-lg shadow-2xl p-4 bg-white max-w-md mx-auto my-10 flex flex-col items-center justify-center'>
       <img src={logo} alt="app logo" className='max-lg:w-16 lg:w-24 mb-1' />
       <h1 className='max-lg:text-xl lg:text-2xl text-blue-600 font-weight-[400]'>Convertly</h1>
       <h4 className='max-lg:text-sm lg:text-xl text-gray-400 font-semibold mb-5'>Currency Converter App</h4>
