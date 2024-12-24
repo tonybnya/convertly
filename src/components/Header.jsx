3485abfda9d8541a46036ba2e74603397cf33d01
tonybnya <nya.tony2010@gmail.com>
2024-12-24 17:26:17 +0100

    style: reduce the size of the logo in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 const Header = () => {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <img src={logo} alt="app logo" className='max-lg:w-16 lg:w-24 mb-4 saturate-200 animate-pulse drop-shadow-other brightness-50' />
+      <img src={logo} alt="app logo" className='max-lg:w-8 lg:w-12 mb-4 saturate-200 animate-pulse drop-shadow-other brightness-50' />
       <button className='bg-black shadow-lg shadow-yellow-500/50 text-yellow-500 font-bold py-2 px-4 rounded-xl'>Convertly</button>
       <h4 className='max-lg:text-sm lg:text-xl text-gray-400 font-semibold mb-5'>Currency Converter App</h4>
     </div>
